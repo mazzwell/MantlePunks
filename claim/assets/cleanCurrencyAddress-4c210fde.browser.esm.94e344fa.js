2d8b2972eae233f4f76398443b862f5d01d099d8
mazzwell <137079218+mazzwell@users.noreply.github.com>
2024-01-03 05:36:46 +0600

    Delete claim directory

--- claim/assets/cleanCurrencyAddress-4c210fde.browser.esm.94e344fa.js
+++
@@ -1,1 +0,0 @@
-import{aB as r,ab as e}from"./index.63d6a091.js";function t(a){return r(a)?e:a}export{t as c};
--- claim/assets/index.919c72e0.js
+++
@@ -1,1 +0,0 @@
-import{g as e,I as r}from"./index.63d6a091.js";const a=e(r);export{a as r};
